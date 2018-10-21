be76fa7aa6d652c04948727477ffd43ec642cba5
Ethan Smoller <ethansmoller@gmail.com>
2018-10-20 22:18:24 -0700

    Version 1!

--- assets/javascript/Game.js
+++ assets/javascript/Game.js
@@ -2,9 +2,9 @@ class Game {
     constructor() {
         this.questions = this.createQuestions();
         this.questionIndex = 0;
-        this.answerChosen = null;
+        this.answerChosen = "";
         this.correctCount = 0;
-        this.incorrectCount = 0;
+        this.state = "question";
     }
 
     /**
@@ -26,7 +26,23 @@ class Game {
             'What object in William Golding\'s "Lord of the Flies" grants its holder the right to speak?',
             ['A conch shell', 'A coconut', 'A starfish', 'An oyster shell'],
             'A conch shell',
-            'conch.jpg')];
+            'conch.jpg'),
+        new Question(
+            'In "The Hunger Games", the weapons tributes scramble for as the games begin are contained in what object?',
+            ['A chest', 'A pit', 'A cornucopia', 'A plate'],
+            'A cornucopia',
+            'cornucopia.jpg'),
+        new Question(
+            'What is the name of the surveillance state in George Orwell\'s dystopian novel "1984"?',
+            ['Big Brother', 'EYE', 'The Panopticon', 'FizzBuzz'],
+            'Big Brother',
+            'big-brother.png'),
+        new Question(
+            'What two words are written on the back of the Hitchhiker\'s Guide to the Galaxy?',
+            ['Carry On', 'No Worries', 'Don\'t Panic', 'Keep Calm'],
+            'Don\'t Panic',
+            'dont-panic.jpg')
+        ];
     }
 
     /**
@@ -34,6 +50,8 @@ class Game {
      * @param {Object} question - Question to display
      */
     displayQuestion(question) {
+        this.changeState("question");
+
         let questionHeading = $("<h2>").text(question.statement);
         let choiceList = $("<ul>").addClass("choice-list");
 
@@ -43,8 +61,22 @@ class Game {
             choiceList.append(choiceListElement);
         }
 
-        $("#question-area").append(questionHeading);
-        $("#question-area").append(choiceList);
+        this.activeElement.append(questionHeading);
+        this.activeElement.append(choiceList);
+    }
+
+    /** 
+     * Compares a player's chosen answer with the question's actual answer
+     * and displays the appropriate result screen
+     */
+    checkAnswer() {
+        if (this.answerChosen === this.currentQuestion.answer) {
+            this.correctCount++;
+            console.log(this.correctCount);
+            this.displayQuestionResults(this.currentQuestion, 0);
+        } else {
+            this.displayQuestionResults(this.currentQuestion, 1);
+        }
     }
 
     /**
@@ -54,6 +86,8 @@ class Game {
      *                             the incorrect response (1), or ran out of time (2)
      */
     displayQuestionResults(question, exitState) {
+        this.changeState("results");
+
         let message = "";
 
         switch(exitState) {
@@ -72,9 +106,70 @@ class Game {
         let correctAnswer = $("<p>").text(`The correct answer was: ${question.answer}`);
         let resultImage = $("<img>").attr("src", `assets/images/${question.image}`);
 
-        $("#question-results-area").append(resultText);
-        if (exitState !== 0) { $("#question-results-area").append(correctAnswer); }
-        $("#question-results-area").append(resultImage);
+        this.activeElement.append(resultText);
+        if (exitState !== 0) { this.activeElement.append(correctAnswer); }
+        this.activeElement.append(resultImage);
+    }
+
+    /** Change game to desired state, clearing the current
+     *  game area and rendering the target area.
+     */
+    changeState(state) {
+        this.activeElement.empty();
+        this.activeElement.css("display", "none");
+        // Hides timer if coming from question state
+        if (game.state === "question") {
+            $("#timer").css("display", "none");
+        }
+
+        this.state = state;
+
+        // Shows timer if going to question state
+        if (game.state === "question") {
+            $("#timer").css("display", "block");
+        }
+
+        this.activeElement.css("display", "block");
+    }
+
+    /** 
+     * Displays game results at the end of a round with a message
+     * based on their percent correct and a button to reset the game.
+     */
+    displayGameResults() {
+        this.changeState("endgame");
+
+        let grade = game.correctCount / game.questions.length;
+        let scoreDisplay = $("<h2>");
+        let assessment = $("<h2>");
+
+        let restartButton = $("<div>Restart</div>");
+        restartButton.addClass("button");
+        restartButton.attr("id", "restart");
+
+        if (grade <= 1 && grade > 0.8) {
+            // A-B
+            assessment.text("Great work!");
+        } else if (grade <= 0.8 && grade > 0.6) {
+            // B-D
+            assessment.text("Not bad!");
+        } else {
+            // F
+            assessment.text("Try again!");
+        }
+        scoreDisplay.html(`Your final score: ${this.correctCount} / ${this.questions.length}`);
+        this.activeElement.append(scoreDisplay);
+        this.activeElement.append(assessment);
+        this.activeElement.append(restartButton);
+    }
+
+    /** Resets the game */
+    reset() {
+        this.questionIndex = 0;
+        this.correctCount = 0;
+
+        this.changeState("question");
+        this.displayQuestion(this.currentQuestion);
     }
 
     /**
@@ -83,5 +178,19 @@ class Game {
      */
     get currentQuestion() {
         return this.questions[this.questionIndex];
+    }
+
+    /**
+     * Gets the current html area associated with the game's state
+     */
+    get activeElement() {
+        switch (this.state) {
+            case "question":
+                return $("#question-area");
+            case "results":
+                return $("#question-results-area");
+            case "endgame":
+                return $("#game-over");
+        }
     }
 }
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,58 @@
-class Question {
-    constructor(questionText, correctAnswer, questionChoices) {
-        this.questionText = questionText;
-        this.correctAnswer = correctAnswer;
-        this.questionChoices = questionChoices;
-    }
-}
+let game = new Game();
+let questionTimer = 10;
+let nextRoundTimer = 3;
+let time;
+let secondTicker;
 
-let Game = {
-    state: "start",
-    correctCount: 0,
-    incorrectCount: 0,
-    changeState: function(state) {
-        this.state = state;
-    }
-}
+document.getElementById("timer").style.display = "none";
+document.getElementById("game-over").style.display = "none";
 
-let questionArray = [
-    new Question(
-        "What is the meaning of life?",
-        "42",
-        [
-            "42",
-            "I don't know",
-            "Chocolate",
-            "Happiness"
-        ]
-    ),
-    new Question(
-        "What year was I born?",
-        "1995",
-        [
-            "1885",
-            "1995",
-            "1990",
-            "Happiness"
-        ]
-    ),
-    new Question(
-        "What is my name?",
-        "Ethan",
-        [
-            "Ethan",
-            "Dave",
-            "Brent",
-            "Happiness"
-        ]
-    )
-];
+function startTimer(maxTime) {
+    time = maxTime;
+    $("#timer").text(`Time remaining: ${time}`);
 
-let index = 0;
-let time = 10;
-let questionTimer;
-
-function askQuestion() {
-    clearInterval(questionTimer);
-    time = 10;
-    startTimer();
-    $("#question-text").text(questionArray[index].questionText);
-    $("#question-choices").empty();
-    for (let i = 0; i < questionArray[index].questionChoices.length; i++) {
-        let choiceListElement = $("<li>");
-        choiceListElement.text(questionArray[index].questionChoices[i]);
-        choiceListElement.addClass("choice");
-        $("#question-choices").append(choiceListElement);
-    }
-}
-
-function startTimer() {
-    $("#time").text(time);
-    questionTimer = setInterval(function() {
+    secondTicker = setInterval(function() {
         time--;
-        $("#time").text(time);
+        $("#timer").text(`Time remaining: ${time}`);
+
         if (time === 0) {
-            index++;
-            askQuestion();
-        } 
+            clearInterval(secondTicker);
+
+            if (game.state === "question") {
+                game.displayQuestionResults(game.currentQuestion, 2);
+                startTimer(nextRoundTimer);
+            } else if (game.state === "results") {
+                game.questionIndex++;
+                if (game.currentQuestion !== undefined) {
+                    game.displayQuestion(game.currentQuestion);
+                    startTimer(questionTimer);
+                } else {
+                    game.displayGameResults();
+                }
+            }
+        }
     }, 1000);
 }
 
-askQuestion();
+// Click start to start game
+document.getElementById("start").addEventListener("click", function() {
+    game.state = "question";
+    $(this).css("display", "none");
+    $("#timer").css("display", "block");
+    game.displayQuestion(game.currentQuestion);
+    startTimer(questionTimer);
+});
 
-$("#question-choices").on("click", ".choice", function() {
-    let userChoice = $(this).text();
-    console.log(userChoice);
-    if (userChoice === questionArray[index].correctAnswer) {
-        console.log("Correct!");
-    } else {
-        console.log("Tough luck, buddy!");
-    }
-    index++;
-    if (index === questionArray.length) {
-        console.log("End game");
-    } else {
-        askQuestion();
-    }
+// Click restart to restart game
+$(document).on("click", "#restart", function() {
+    game.reset();
+    startTimer(questionTimer);
+});
+
+// Checks player answer when choice is clicked
+$(document).on("click", ".choice", function() {
+    clearInterval(secondTicker);
+    game.answerChosen = $(this).text();
+    game.checkAnswer();
+    startTimer(nextRoundTimer);
 });
